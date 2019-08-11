a82784cdc542698e230823ef6d82c352534dc5b8
tabularasa066a <tabularasa066a@gmail.com>
2019-08-12 05:51:57 +0900

    revised

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,17 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
+    redirect_to root_url and return unless User.where(activated: true)
   end
 
   def create
@@ -50,24 +52,24 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-  # beforeアクション
-  # ログイン済みユーザかチェック
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in"
-      redirect_to login_url
+    # beforeアクション
+    # ログイン済みユーザかチェック
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
     end
-  end
 
-  # 正しいユーザか確認
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    # 正しいユーザか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
-  # 管理者かどうか確認
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://sampleappdayo.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {git
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
