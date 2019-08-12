4c2953942e6b6078cf8745fc9ea98024ef409974
tabularasa066a <tabularasa066a@gmail.com>
2019-08-12 10:06:21 +0900

    revised2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.where(activated: true)
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
