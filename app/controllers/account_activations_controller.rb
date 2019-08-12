27515e6d25cfb47665d502bdb72624291c5f3454
tabularasa066a <tabularasa066a@gmail.com>
2019-08-12 19:05:27 +0900

    Add password reset

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,7 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+    if (user && !user.activated? && user.authenticated?(:activation, params[:id]))
       user.activated
       user.update_attribute(:activated, true)
       user.update_attribute(:activated_at, Time.zone.now)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/ne'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
 
@@ -14,4 +16,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
